a62b01833b8fc4aa58f30f52b9b153e72cccd1f3
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-11-26 16:37:03 +0700

    fix: timestamp bug

--- src/twophase/TwoPhaseTransaction.py
+++ src/twophase/TwoPhaseTransaction.py
@@ -1,14 +1,13 @@
 from cores.Transaction import Transaction
-import timeit
 import time
 
 class TwoPhaseTransaction(Transaction):
     def __init__(self, id: str) -> None:
         super().__init__(id)
-        self.__timestamp = timeit.timeit()
+        self.__timestamp = time.time()
 
         # solve bug when timestamp for 2 transaction is equal
-        time.sleep(0.01)
+        time.sleep(1e-7)
 
     def get_timestamp(self):
         return self.__timestamp
